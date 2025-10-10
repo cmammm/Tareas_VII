3cc46001d1aa631e7a8a781a784af5f0de4be8b2
cmammm <2233072987@correo.ler.uam.mx>
2025-10-10 11:18:03 -0600

    clase 10/10/25

---
+++ script.js
@@ -0,0 +1,3 @@
+let texto = '{"cuerpos":["ectomorfo", "mesomorfo", "endomorfo"], "posicion": ["parado", "sentado", "acostado]}';
+const info = JSON.parse(texto);
+document.getElementById("hola").innerHTML="cuerpo" + info.cuerpos[0] + " " + info.posicion[0];
