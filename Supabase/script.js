a027a214bd6c69c341d9985cbcc2e04b3047c52a
cmammm <2233072987@correo.ler.uam.mx>
2025-11-07 11:28:42 -0600

    h

--- Supabase/script.js
+++ Supabase/script.js
@@ -1,4 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+//import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://xyzcompany.supabase.co', 'publishable-or-anon-key')
+const supabaseClient = supabase.createClient('https://tqtcyphinjsqxuadxtwj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxdGN5cGhpbmpzcXh1YWR4dHdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MDE0MzUsImV4cCI6MjA3NzQ3NzQzNX0.MlgyjJM67TvOaDnL0V_As3IpZ8EkoofQBNa8jrEkp3E')
+
+console.log(supabaseClient)
