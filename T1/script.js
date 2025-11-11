a88ce0fb7188a23381f34b9fa615c7c278cacb32
cmammm <2233072987@correo.ler.uam.mx>
2025-11-10 22:31:31 -0600

    t1

--- T1/script.js
+++
@@ -1,3 +0,0 @@
-let texto = '{"cuerpos":["ectomorfo", "mesomorfo", "endomorfo"], "posicion": ["parado", "sentado", "acostado]}';
-const info = JSON.parse(texto);
-document.getElementById("hola").innerHTML="cuerpo" + info.cuerpos[0] + " " + info.posicion[0];
